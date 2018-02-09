a6115750c7a6b33ce92a08ba08816c5f2b41db16
romanpotapov <realpotapov@gmail.com>
2018-02-09 21:54:31 +0200

    add object type

--- lessons/index.js
+++ lessons/index.js
@@ -1,19 +1,33 @@
-var rand = Math.random() * 10;
-console.log(rand);
-var coins = Math.round(rand);
-console.log(coins);
-if(coins %2 === 0) {
-    console.log('even');
-}
-else {
-    console.log('odd');
-}
+// var rand = Math.random() * 10;
+// console.log(rand);
+// var coins = Math.round(rand);
+// console.log(coins);
+// if(coins %2 === 0) {
+//     console.log('even');
+// }
+// else {
+//     console.log('odd');
+// }
+//
+// switch (coins) {
+//     case 1:
+//         console.log('sdfd');
+//         break;
+//
+//     default:
+//     console.log('other');
+// }
 
-switch (coins) {
-    case 1:
-        console.log('sdfd');
-        break;
 
-    default:
-    console.log('other');
-}
+// object
+
+const student = {
+    name: 'Dmitrii',
+    secondName: 'Voytenko',
+    fatherName: 'Yurievich',
+    dateOfBirth: 2001
+
+};
+
+console.log(student.name + ' ' +student.secondName);
+console.log(`His second name is ${student.secondName}`);
