51569691ad367cf9729e7ed6bcfa55c07c9c1727
potapovroma <potapowroma@gmail.com>
2018-03-15 17:49:47 +0200

    started work on home work

--- hw-6/js/index.js
+++ hw-6/js/index.js
@@ -1,25 +1,67 @@
-let keyTrainer = {
+const keyTrainer = {
   chars: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
-  //charCount:
-  setCharCount() {
-    do {
-    var count = prompt('Enter the number of characters: - ');
-    if(count == null) break;
-    console.log(count);
-    count = keyTrainer.checkPositiveInteger(count);
-    console.log(count);
-
-  } while(count);
-  console.log('finished');
+  charCount: '',
+  setCharCount: function (){
+    let characters = prompt('Введите количество символов: ');
+    while(this.checkPositiveInteger(characters)){
+      characters = prompt('Введите количество символов: ');
+    }
+    this.charCount = +characters;
   },
-
-  checkPositiveInteger(number) {
-    if(number >= 0 && ((number ^ 0) == number)) {
+  checkPositiveInteger: function (str){
+    let num = +str;
+    if(num !== parseInt(num) || num < 0){
+      return true;
+    }else{
       return false;
     }
-    else {
-      return true;
-    }
-  }
+  },
+  task: '',
+  createTask: function (){
+    let arr = new Array(this.charCount);
 
+    for(let i = 0; i < this.charCount; i++){
+      arr[i] = this.chars[Math.floor(Math.random() * this.chars.length)];
+    }
+
+    this.task = arr;
+  },
+  startTask: function (){
+    let str = prompt(`Наберите строку: '${this.task.join('')}'`, 'Введите строку');
+    let arr = str.split('');
+    let value = 0;
+
+    if(arr.length > this.task.length){
+      for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== this.task[i]){
+          value += 1;
+        }
+      }
+    }else{
+      for(let i = 0; i < this.task.length; i++){
+        if(this.task[i] !== arr[i]){
+          value += 1;
+        }
+      }
+    }
+    this.userInput = str;
+    this.userErrors = value;
+    if(value === 0){
+      console.log(`Поздравляем вы ввели правильно строку!`);
+    }else{
+      console.log(`Вы допустили ${value} ошибок. Попробуйте снова!`);
+    }
+  },
+  userInput: '',
+  userErrors: ''
+}
+
+/*==================== Check ====================*/
+run();
+
+/*==================== Function ====================*/
+function run(){
+  keyTrainer.setCharCount();
+  keyTrainer.createTask();
+  keyTrainer.startTask();
 }
